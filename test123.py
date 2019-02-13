43445771a8de5ad17b2fbdd9d9c6dfefb194c523
BenGreenhill <bgreenhill1@sheffield.ac.uk>
2019-02-13 10:46:46 +0000

    added distance tester file

---
+++ test123.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+'''COM2009-3009 EV3DEV TEST PROGRAM'''
+
+# Connect left motor to Output C and right motor to Output B
+# Connect an ultrasonic sensor to Input 3
+
+import os
+import sys
+import time
+import ev3dev.ev3 as ev3
+
+# state constants
+ON = True
+OFF = False
+
+
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def reset_console():
+    '''Resets the console to the default state'''
+    print('\x1Bc', end='')
+
+
+def set_cursor(state):
+    '''Turn the cursor on or off'''
+    if state:
+        print('\x1B[?25h', end='')
+    else:
+        print('\x1B[?25l', end='')
+
+
+def set_font(name):
+    '''Sets the console font
+
+    A full list of fonts can be found with `ls /usr/share/consolefonts`
+    '''
+    os.system('setfont ' + name)
+
+
+def main():
+    '''The main function of our program'''
+
+    # set the console just how we want it
+    reset_console()
+    set_cursor(OFF)
+    set_font('Lat15-Terminus24x12')
+
+    # announce program start
+    ev3.Sound.speak('Hey whatsup dude').wait()
+
+    # set the motor variables
+    mb = ev3.LargeMotor('outB')
+    mc = ev3.LargeMotor('outC')
+    us3 = ev3.UltrasonicSensor('in3')
+    sp = -10
+    kp = 1000
+    ki = 100
+    kd = 10000
+    integral = 0
+    derivative = 0
+    offset = 100
+    lastError = 0
+
+    while True:
+        time.process_time() 
+        ds = us3.value()
+        error = ds - offset
+        integral = integral + error
+        print(integral)
+        derivative = error - lastError
+        print(derivative)
+        change = kp*error + ki*integral + kd*derivative
+        change = change/100
+        mb.run_direct(duty_cycle_sp=sp+change)
+        mc.run_direct(duty_cycle_sp=-sp-change)
+
+
+#if __name__ == '__main__':
+#   main()
